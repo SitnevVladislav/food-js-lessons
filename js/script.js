9189f1cc12f06528d12145c66aa9c65aae027f2b
Vlad <SitnevVladislav>
2021-03-25 18:44:24 +0600

    closing modal message added

--- js/script.js
+++ js/script.js
@@ -98,8 +98,7 @@ window.addEventListener('DOMContentLoaded', function() {
     // Modal
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal');
 
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', openModal);
@@ -118,10 +117,9 @@ window.addEventListener('DOMContentLoaded', function() {
         clearInterval(modalTimerId);
     }
     
-    modalCloseBtn.addEventListener('click', closeModal);
 
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute('data-close') =='') {
             closeModal();
         }
     });
@@ -251,15 +249,38 @@ window.addEventListener('DOMContentLoaded', function() {
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    showThanksModal(message.success)
                     form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
+                    statusMessage.remove();
                 } else {
-                    statusMessage.textContent = message.failure;
+                    showThanksModal(message.failure);
                 }
             });
         });
+    }
+
+    function showThanksModal(message){
+         const prevModalDialog  = document.querySelector('.modal__dialog');
+
+         prevModalDialog.classList.add('hide');
+         openModal();
+
+         const thanksModal = document.createElement('div');
+         thanksModal.classList.add('modal__dialog');
+         thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+         `;
+
+         document.querySelector('.modal').append(thanksModal);
+
+         setTimeout(()=>{
+             thanksModal.remove();
+             prevModalDialog.classList.toggle('show');
+             prevModalDialog.classList.toggle('hide');
+             closeModal();
+         },4000);
     }
 });
